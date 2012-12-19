7ac09477128755062e07d08069b445e05c295c12
geldshot <geldshot@gmail.com>
2012-12-18 19:03:54 -0800

    AuthorizeAPI Process
    finished initial process() implementation for 4 web input commands.

--- webroot/Authorize/authorizeAPI.php
+++ webroot/Authorize/authorizeAPI.php
@@ -16,30 +16,49 @@ class AuthorizeAPI{
 		$this->webInput = $webInput;
 		switch($webInput['command'])
 			case "login":
-				if(isset($webInput['username'])){
-					$name = $webInput['username'];
-					$type = 1;
+				if(isset($webInput['username']) && isset($webInput['password'])){
+					if(($this->model->login($webInput['username'], $webInput['password']))
+						$outPut = "success";
+					else
+						$outPut = "AUTHORIZEAPI: login failed";
 				}
-				else if(isset($webInput['username'])){
-				
-				}
-				if(($this->model->login($webInput['username'], $webInput['password']))
-					$outPut = "success";
 				else
-					$outPut = "AUTHORIZEAPI: login failed";
+					$outPut = "AUTHORIZEAPI: missing login information";
 				break;
+				
 			case "register":
-				if(($this->model->register($webInput['email'], $webInput['username'], $webInput['password']))
-					$outPut = "success";
+				if(isset($webInput['email']) && isset($webInput['username']) && isset($webInput['password'])){
+					if(($this->model->register($webInput['email'], $webInput['username'], $webInput['password']))
+						$outPut = "success";
+					else
+						$outPut = "AUTHORIZEAPI: login failed";
+					}
 				else
-					$outPut = "AUTHORIZEAPI: login failed";
+					$outPut = "AUTHORIZEAPI: missing registration information";
 				break;
-				
-		
-		
-		
-		
-		
+			case "changepass":
+				if(isset($webInput['username']) && isset($webInput['email']) && isset($webInput['key']) && isset($webInput['password'])){
+					if(($this->model->login($webInput['username'], $webInput['password']))
+						$outPut = "success";
+					else
+						$outPut = "AUTHORIZEAPI: changepass failed";
+				}
+				else
+					$outPut = "AUTHORIZEAPI: missing changepass information";
+				break;
+			case "passrequest":
+				if(isset($webInput['username'])){
+					if(($this->model->request($webInput['username']))
+						$outPut = "success";
+					else
+						$outPut = "AUTHORIZEAPI: request failed";
+				}
+				else
+					$outPut = "AUTHORIZEAPI: missing request information";
+				break;
+			default:
+				$outPut = "AUTHORIZEAPI: invalid command";
+				break;
 	}
 	
 	public function Error(){
